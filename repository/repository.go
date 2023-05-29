f618420f83209c160f550ea771f1fdc3759679a4
juliocnsouzadev <juliocnsouzadev@gmail.com>
2023-05-29 06:22:53 +0100

    chore: remove commented code in repository

--- repository/repository.go
+++ repository/repository.go
@@ -3,7 +3,4 @@ package repository
 type Repository[T any] interface {
 	Insert(t T) error
 	FindByTopic(topic string) ([]T, error)
-	//FindById(id string) T
-	//Update(t T) (T, error)
-	//Delete(id int)
 }
